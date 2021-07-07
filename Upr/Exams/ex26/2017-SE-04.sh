2e3da02b57276c3162cd130586ac63c5f105aad1
Radina <radina.nuneva@gmail.com>
2021-07-07 12:19:03 +0300

    Update 2017-SE-04.sh

--- Upr/Exams/ex26/2017-SE-04.sh
+++ Upr/Exams/ex26/2017-SE-04.sh
@@ -1,87 +1,35 @@
- #You cannot escape a single quote inside a pair of singe quotes in shell. 
- #Escaping double quotes is allowed though.
- #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- #sed 's/\'//g' NE
- #sed "s/'//g" DA
- 
- Напишете shell script, който получава задължителен първи позиционен параметър
-– директория и незадължителен втори – име на файл. Скриптът трябва да намира в подадената
-директория и нейните под-директории всички symlink-ове и да извежда (при подаден аргумент
-файл – добавяйки към файла, а ако не е – на стандартния изход) за тях следната информация:
-• ако destination-a съществува – името на symlink-а -> името на destination-а;
-• броя на symlink-овете, чийто destination не съществува.
-Примерен изход:
-lbaz -> /foo/bar/baz
-lqux -> ../../../qux
-lquux -> /foo/quux
-Broken symlinks: 34
+#!/bin/bash
 
-$1 - dir
-$2 - file
+# $1 - dir
+# $2 - file (optional)
 
-$counter=0;
-find $1 -type l | while read line; do #namira vs symlinkove
-  $reference=$(stat $line --printf "%N\n" | sed "s/'//g")
-  dest=$(echo $reference | awk -F ' -> ' '{print $2}')
-  if [ ! -e $dest ]; do
-    counter++
-  else
-     if [ '$2' -ne '' ]; do
-        echo "$reference\n" >> $2
-     else
-        echo "$reference\n"
-     done
-  done
-done
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+        exit 1
+fi
 
-if [ '$2' -ne '' ]; do
-  echo "Broken symlinks: $counter\n" >> $2
+if [ ! -d $1 ]; then
+        exit 2
+fi
+
+counter=0
+
+while read line; do
+        connection=$(stat $line --printf "%N\n" | sed "s/'//g")
+        target=$(echo $connection | awk -F ' -> ' '{print $2}')
+
+        if [ ! -e $target ]; then
+                counter=$((counter+1))
+        else
+                if [ $# -eq 2 ]; then
+                        echo "$connection" >> $2
+                else
+                        echo "$connection"
+                fi
+        fi
+done < <(find $1 -type l)
+
+if [ $# -eq 2 ]; then
+        echo "Broken symlinks: $counter" >> $2
 else
-  echo "Broken symlinks: $counter\n"
-done
-
-if [dest exists] 
-  -> echo 'symlink -> dest'
-else
-  ->
-    counter++;
- 
-echo "Broken symlinks: $counter"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        echo "Broken symlinks: $counter"
+fi
